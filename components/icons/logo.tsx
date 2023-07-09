b6af64fff152a6936a967dd3c102abec64ed4794
Marius Paduraru <31074930+wisethee@users.noreply.github.com>
2023-07-09 11:21:22 +0100

    feat: adding basic desktop navigation and the hero section

---
+++ components/icons/logo.tsx
@@ -0,0 +1,25 @@
+const Logo = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={64} height={64} fill="none">
+    <path
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={1.5}
+      strokeWidth={2}
+      d="M35.91 23.852 22.221 12.444 2.666 51.555 35.91 23.852ZM41.777 32l19.556 13.037-27.378-9.126L41.777 32Z"
+      clipRule="evenodd"
+    />
+    <path
+      fill="#000"
+      fillRule="evenodd"
+      stroke="#000"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeMiterlimit={1.5}
+      strokeWidth={2}
+      d="m2.666 51.555 39.11-32.592 19.556 26.074L41.777 32 2.666 51.555Z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default Logo;
